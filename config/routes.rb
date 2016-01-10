c5a506251d1313d2884c3a5b40ad2b797c9ad00e
Leo Koo <leokoo@gmail.com>
2016-01-10 21:42:44 +0800

    modded the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  resources :links, only: [:show, :new, :create] do
-    resource :upvote, only: [:create]
-    resource :downvote, only: [:create]
-  end
+  # resources :links, only: [:show, :new, :create] do
+  #   resource :upvote, only: [:create]
+  #   resource :downvote, only: [:create]
+  # end
 
-  namespace :api do
-    namespace :v1 do
-      resources :links, only: [:index, :create]
-    end
-  end
-  
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :links, only: [:index, :create]
+  #   end
+  # end
+
+  resources :links
+
   root to: "links#index"
 end
